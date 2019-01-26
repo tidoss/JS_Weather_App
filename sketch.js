c9cb14a4e3b5a4c478d37b524c57b78a97645ef6
Stanimir Kalchev <stan.k.kalchev@gmail.com>
2019-01-26 22:51:32 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,78 @@
+function detectmob() {
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+
+var isMobi = detectmob();
+
+var numParticles;
+
+if (isMobi) {
+  numParticles = 400
+} else {
+  numParticles = 1000
+}
+
+var inc = 0.1;
+var scl = 10;
+var cols, rows;
+var zoff = 0;
+var fr;
+var particles = [];
+var flowfield;
+var stan;
+
+function setup() {
+  stan = createCanvas(windowWidth, windowHeight);
+  stan.position(0,0);
+  stan.style('z-index', '-1');
+  colorMode( 225,225,225);
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  fr = createP('');
+
+  flowfield = new Array(cols * rows);
+
+  for (var i = 0; i < numParticles; i++) {
+    particles[i] = new Particle();
+  }
+}
+
+function draw() {
+  var yoff = 0;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      flowfield[index] = v;
+      xoff += inc;
+    }
+    yoff += inc;
+    zoff += 0.0003;
+  }
+
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].edges();
+    particles[i].show();
+  }
+}
+
+function windowResized() {
+   resizeCanvas(windowWidth, windowHeight);
+}
